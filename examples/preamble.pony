b19416ddfb835a9e2db14c978465b9e1b9a4b14c
Joel Lienhard <joel.lienhard@stud.uni-hannover.de>
2026-03-25 17:12:20 +0100

    implement inheritance

--- examples/preamble.pony
+++ examples/preamble.pony
@@ -1,3 +1,8 @@
 actor Main
   new create(env: Env) =>
-    env.out.print(Examples.list("foo", ["ba1"; "ba2"]))
+    env.out.print(Examples.list(["ba1"; "ba2"], "foo"))
+    env.out.print(Examples.complex("some text"))
+    env.out.print(Examples.complex(42))
+    env.out.print(Examples.complex([1; 1; 2; 3; 5; 8]))
+    env.out.print(Examples.if_example(-3))
+    env.out.print(Examples.login_form())
--- src/comptempl.nim
+++ src/comptempl.nim
@@ -1,11 +1,12 @@
-import std/[options, sequtils, strutils, parseutils, strformat, paths, files, dirs, cmdline]
+import std/[options, sequtils, strutils, parseutils, strformat, tables, paths, files, dirs, cmdline]
 import fusion/matching
 
 
 type
   TokenKind = enum
     tkText, tkExpr
-    tkFun="fun", tkFor="for", tkIf="if", tkElseif="elseif", tkElse="else", tkMatch="match"
+    tkFun="fun", tkExtends="extends", tkBlock="block"
+    tkFor="for", tkIf="if", tkElseif="elseif", tkElse="else", tkMatch="match"
     tkCase="|", tkEnd="end"
 
   Token = object
@@ -17,7 +18,7 @@ type
     cond: string
     body: seq[Node]
 
-  NodeKind = enum nkText, nkExpr, nkIf, nkFor, nkMatch
+  NodeKind = enum nkText, nkExpr, nkIf, nkFor, nkMatch, nkBlock
   Node = ref object
     case kind: NodeKind
     of nkText, nkExpr: val: string
@@ -31,20 +32,33 @@ type
       matchOn: string
       cases: seq[Branch]
       matchElseBody: Option[seq[Node]]
+    of nkBlock:
+      blockName: string
+      blockBody: seq[Node]
 
   Template = object
-    funHead: string
-    body: seq[Node]
+    funHead: Option[string]
+    case isExtending: bool
+    of true:
+      extends: string
+      extendsDefPos: int
+      blocks: Table[string, seq[Node]]
+    of false:
+      body: seq[Node]
 
-  SyntaxError = ref object of CatchableError
+  CompileError = ref object of CatchableError
+    filename: string
     pos: int
 
 
 func textToken(pos: int, text: string): Token {.inline.} =
   Token(pos: pos, kind: tkText, val: text)
 
-proc lex(code: string): seq[Token] =
+proc lex(code, filename: string): seq[Token] =
   var i = code.skipWhitespace()
+
+  template error(posDef: int, msgDef: string) =
+    raise CompileError(filename: filename, pos: posDef, msg: msgDef)
 
   proc tryParseCtrl: Option[Token] =
     inc i
@@ -71,9 +85,16 @@ proc lex(code: string): seq[Token] =
         let j = s.parseUntil(keyword, Whitespace) + 1
         try:
           token.kind = parseEnum[TokenKind](keyword)
-          token.val = s[j..^1]
+          token.val = s[j..^1].strip()
+          case token.kind
+          of tkEnd, tkElse:
+            error(j, &"unexpected '{token.val}' after '{keyword}'")
+          of tkText, tkExpr, tkCase:
+            error(j, &"unexpected '{s}'")
+          else:
+            discard
         except ValueError:
-          raise SyntaxError(pos: i, msg: &"unknown keyword '{keyword}'")
+          error(i, &"unknown keyword '{keyword}'")
     else:
       return
     
@@ -87,7 +108,7 @@ proc lex(code: string): seq[Token] =
     i += code.skipUntil('{', i)
     let textEnd = i #potentually
     if Some(@token) ?= tryParseCtrl():
-      if textStart < textEnd-1:
+      if textStart < textEnd:
         tokens &= textToken(textStart, code[textStart ..< textEnd])
       tokens &= token
       textStart = i
@@ -100,19 +121,52 @@ proc lex(code: string): seq[Token] =
   tokens
 
 
-func error(token: Token, msg: string): SyntaxError {.inline.} =
-  SyntaxError(pos: token.pos, msg: msg)
+proc parse(
+  tokens: seq[Token],
+  filename: string,
+  isPartial = false
+): Template =
 
-proc parse(tokens: seq[Token]): Template =
+  var i = 0
 
   if len(tokens) == 0:
-    raise SyntaxError(pos: 0, msg: "empty file")
-  if tokens[0].kind != tkFun:
-    raise tokens[0].error("template file must start with a 'fun' definition")
+    raise CompileError(filename: filename, pos: 0, msg: "empty file")
 
-  var i = 1
+  template error(token: Token, msgDef: string) =
+    raise CompileError(filename: filename, pos: token.pos, msg: msgDef)
+  
+  # get funtion head
+  let funHead =block:
+    let token = tokens[0]
+    if token.kind == tkFun:
+      if isPartial:
+        token.error("partial templates should have no 'fun' declaration")
+      inc i
+      some(token.val)
+    elif isPartial:
+      none(string)
+    else:
+      token.error(
+        &"non-partial template file must start with a 'fun' declaration. If '{filename}' should be partial name it '_{filename}'")
 
-  proc parseNodes(stopOn: set[TokenKind] = {}): seq[Node] =
+  # get extends
+  result = 
+    if len(tokens) > i and (let token = tokens[i]; token.kind == tkExtends):
+      inc i
+      Template(isExtending: true, extends: token.val, extendsDefPos: token.pos)
+    elif (
+      len(tokens) > i+1 and
+      (let token = tokens[i+1]; token.kind == tkExtends and
+      tokens[i].kind == tkText and tokens[1].val.allIt(it in Whitespace))
+    ):
+      i += 2
+      Template(isExtending: true, extends: token.val, extendsDefPos: token.pos)
+    else:
+      Template(isExtending: false)
+
+  result.funHead = funHead
+
+  proc parseNodes(stopOn: set[TokenKind] = {}, needsClosingEnd=true): seq[Node] =
     while i < len(tokens):
       let token = tokens[i]
       inc i
@@ -166,20 +220,107 @@ proc parse(tokens: seq[Token]): Template =
 
           node
 
-        of tkEnd: return
+        of tkBlock:
+          Node(kind: nkBlock, blockName: token.val, blockBody: parseNodes())
+
+        of tkEnd:
+          if not needsClosingEnd:
+            token.error("unexpected 'end'")
+          return
 
         else:
           if token.kind in stopOn:
             dec i
             return
-          raise token.error(&"unexpected '{token.kind}'")
+          token.error(&"unexpected '{token.kind}'")
 
-  Template(funHead: tokens[0].val, body: parseNodes())
+    if needsClosingEnd:
+      raise CompileError(
+        filename: filename, pos: -1,
+        msg: "unexpected end of file. forgot a closing 'end' ?"
+      ) #todo correct `pos`
+
+  if not result.isExtending:
+    result.body = parseNodes(needsClosingEnd=false)
+
+  else:
+    while i < len(tokens):
+      let token = tokens[i]
+      inc i
+      case token.kind
+      of tkBlock:
+        if token.val in result.blocks:
+          token.error(&"double definition of block '{token.val}'")
+        result.blocks[token.val] = parseNodes()
+
+      of tkText:
+        if not token.val.allIt(it in Whitespace):
+          token.error("extending templates should only contain block definitions")
+      else:
+        token.error(&"unexpected '{token.kind}'")
+
+  assert i >= high(tokens)
+
+
+func replaceBlocks(templ: Template, blocks: Table[string, seq[Node]]): Template =
+  proc replaceBlocks(nodes: seq[Node]): seq[Node] =
+    result = @[]
+    for node in nodes:
+      case node.kind
+      of nkText, nkExpr: result &= node
+      of nkBlock:
+        if node.blockName in blocks:
+          result &= blocks[node.blockName]
+        else:
+          result &= Node(
+            kind: nkBlock,
+            blockName: node.blockName,
+            blockBody: replaceBlocks(node.blockBody)
+          )
+      of nkFor:
+        result &= Node(
+          kind: nkFor,
+          forIter: node.forIter,
+          forBody: replaceBlocks(node.forBody)
+        )
+      of nkIf:
+        result &= Node(
+          kind: nkIf,
+          ifElseBody: replaceBlocks(node.ifElseBody),
+          ifBranches: node.ifBranches.mapIt((it.cond, replaceBlocks(it.body)))
+        )
+      of nkMatch:
+        result &= Node(
+          kind: nkMatch,
+          matchOn: node.matchOn,
+          matchElseBody: node.matchElseBody.map(replaceBlocks),
+          cases: node.cases.mapIt((it.cond, replaceBlocks(it.body)))
+        )
+
+  assert not templ.isExtending
+  Template(isExtending: false, body: replaceBlocks(templ.body))
+
+proc expandExtension(
+  filename: string,
+  templ: var Template,
+  ctx: var Table[string, Template]
+) =
+  if templ.isExtending:
+    if templ.extends notin ctx:
+      raise CompileError(
+        filename: filename, pos: templ.extendsDefPos,
+        msg: &"'{templ.extends}' not found"
+      )
+    expandExtension(templ.extends, ctx[templ.extends], ctx)
+    let funHead = templ.funHead
+    templ = replaceBlocks(ctx[templ.extends], templ.blocks)
+    templ.funHead = funHead
 
 
 proc generate(temp: Template): string =
   
-  var code = &"  fun {temp.funHead}: String => "
+  assert temp.funHead.isSome
+  var code = &"  fun {temp.funHead.get}: String => "
 
   var tmpId = 0
   proc tmpVar: string =
@@ -223,6 +364,9 @@ proc generate(temp: Template): string =
           generate(body)
         code &= " end)"
 
+      of nkBlock:
+        generate(node.blockBody)
+
   generate(temp.body)
   code & "\n"
 
@@ -243,17 +387,32 @@ func toPascalCase(s: string): string =
 proc compile(folder: Path) =
   var code = ""
 
-  const preambleFilename = "preamble.pony"
-  let preamblePath = folder/Path(preambleFilename)
-  if fileExists(preamblePath):
-    code &= readFile($preamblePath) & "\n\n"
+  try:
+    const preambleFilename = "preamble.pony"
+    let preamblePath = folder/Path(preambleFilename)
+    if fileExists(preamblePath):
+      code &= readFile($preamblePath) & "\n\n"
 
-  code &= "primitive " & folder.lastPathPart().`$`.toPascalCase() & "\n"
+    var templs: Table[string, Template]
+    for kind, path in walkDir(folder):
+      let filename = $path.lastPathPart()
+      if kind == pcFile and filename != preambleFilename:
+        templs[filename] =
+          readFile($path)
+          .lex(filename)
+          .parse(filename, isPartial=filename.startsWith("_"))
 
-  for kind, path in walkDir(folder):
-    if kind == pcFile and $path.lastPathPart() != preambleFilename:
-      code &= '\n' & generate(readFile($path).lex.parse)
+    for filename, templ in templs.mpairs:
+      expandExtension(filename, templ, templs)
 
+    code &= "primitive " & folder.lastPathPart().`$`.toPascalCase() & "\n"
+    for templ in templs.values:
+      if templ.funHead.isSome:
+        code &= '\n' & generate(templ)
+
+  except CompileError as e:
+    quit &"{e.filename}({e.pos}) {e.msg}"
+      
   writeFile($folder.changeFileExt("pony"), code)
 
 
